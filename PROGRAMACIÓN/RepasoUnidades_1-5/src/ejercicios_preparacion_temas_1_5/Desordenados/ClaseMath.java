423163e5c3a525ca78eb83b5e22243f8826b39ca
Ignacio Rueda <105673585+Ignacio-Rueda@users.noreply.github.com>
2024-01-20 06:35:50 +0100

    Ordenando temas.

---
+++ PROGRAMACIÓN/RepasoUnidades_1-5/src/ejercicios_preparacion_temas_1_5/Desordenados/ClaseMath.java
@@ -0,0 +1,37 @@
+package ejercicios_preparacion_temas_1_5.Desordenados;
+import java.util.Random;
+public class ClaseMath {
+
+	public static void main(String[]args) {
+		
+		
+		System.out.printf("Devuelve un número aleatorio entre 0.0 y menor que 1.0 = '%.2f' %n",Math.random());
+		System.out.printf("Devuelve un número aleatorio entre 0.0 y el número que deseamos, en este caso INFERIOR A 25 = '%.2f' %n",Math.random()*25);
+		System.out.printf("Devuelve un número aleatorio ENTERO 0 y el número que deseamos, en este caso INFERIOR A 25 = '%d' %n",(int)(Math.random()*25));
+		
+		System.out.println("Queremos alcanzar también el número 25");
+		System.out.printf("Devuelve un número aleatorio ENTERO 0 y el número que deseamos, en este caso INFERIOR O IGUAL 25 = '%d' %n",(int)(Math.random()*26));
+		System.out.printf("Devolver un número aleatorio ENTERO entre 1 y 25 %d%n",1+(int)(Math.random()*26));//Sumamos al origen, la cantidad de desplazamiento.
+		System.out.println(65+(int)(Math.random()*25));
+
+		//---------------------------//
+		//UTILIZAMOS LA CLASE RANDOM//
+		//-------------------------//
+		System.out.println("Utilizamos la clase Random para generar un número aleatorio entre 0 y 25");
+		//Creamos el objeto/instancia de la clase random usando el constructor sin parámetros.
+		Random numAleatorio = new Random();
+		System.out.println(numAleatorio.nextInt(26));//Devuelve número aleatorio entre 0 (inclusivo) y el número que le pasamos como parámetro (exclusivo).
+		System.out.println(65 + numAleatorio.nextInt(26));
+
+		
+		//Crea un número entre 10 y 20 incluidos
+		Random miNumeroAleatorio = new Random();
+		
+		System.out.println(10 +(int) (Math.random()*11));
+		System.out.println(10 + miNumeroAleatorio.nextInt(11));
+
+
+
+	
+	}
+}
---
+++ PROGRAMACIÓN/RepasoUnidades_1-5/src/ejercicios_preparacion_temas_1_5/Desordenados/EjemploRectangle.java
@@ -0,0 +1,23 @@
+package ejercicios_preparacion_temas_1_5.Desordenados;
+import java.awt.Rectangle;
+public class EjemploRectangle {
+
+	public static void main(String[] args) {
+		Rectangle r1,r2;
+		r1 = new Rectangle(0,0,10,5);//Vértice inferior izquierdo en ubicado en coordenadas 0,0. Base longitud 10 y altura longitud 5.
+		r2 = r1;
+		Rectangle r3 = new Rectangle(0,0,10,5);
+
+		int edad = -34;
+	
+		System.out.println(Math.PI);
+		System.out.println(System.currentTimeMillis());
+		System.out.println(System.getProperty("file.separator"));
+		System.out.println(System.getProperty("user.language"));
+		System.out.println(System.getProperty("java.version"));
+		System.out.println("GENERACIÓN NÚMEROS ALEATORIOS CLASE MATH");
+		
+		System.out.println((int)Math.random());
+	}
+
+}
