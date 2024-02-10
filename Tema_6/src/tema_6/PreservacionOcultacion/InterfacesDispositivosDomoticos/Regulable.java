c4f7613ce67e81012e69b4ba04f499a799ce43a1
Ignacio Rueda <105673585+Ignacio-Rueda@users.noreply.github.com>
2024-02-10 11:22:11 +0100

    Delete Tema_6 directory

--- Tema_6/src/tema_6/PreservacionOcultacion/InterfacesDispositivosDomoticos/Regulable.java
+++
@@ -1,9 +0,0 @@
-
-package tema_6.PreservacionOcultacion.InterfacesDispositivosDomoticos;
-
-public interface Regulable {
-    public int getIntensidad();
-    public int up();
-    public int down();
-    
-}
