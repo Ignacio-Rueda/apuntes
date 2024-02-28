8e3c1c78023c7f05456c5c8c95613300c68446be
Ignacio-Rueda <nachord@outlook.com>
2024-02-28 08:19:35 +0100

    Inicio mapas

--- PROGRAMACIÓN/Tema_7/src/tema_7/Mapas/Mapas.java
+++ PROGRAMACIÓN/Tema_7/src/tema_7/Mapas/Mapas.java
@@ -1,10 +1,43 @@
 
 package tema_7.Mapas;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Mapas {
     public static void main(String[]args){
-        System.out.println("hola ma!");
-        System.out.println("hola ");
+        //CREACIÓN MAPA.
+        Map<String,String> codigoCliente;
+        codigoCliente = new HashMap<>();
+        
+        //INSERTAR DATOS.
+        codigoCliente.put("Nacho", "001");
+        codigoCliente.put("Ramón","002");
+        codigoCliente.put("Eva", "003");
+        
+        System.out.println("MOSTRAR DATOS MEDIANTE CLAVE");
+        System.out.printf("El código del cliente es: %s\n",codigoCliente.get("Ramón"));
+        System.out.println();
+
+        System.out.println("MOSTRAR DATOS EN CONJUNTO");
+        for(Map.Entry<String,String> pares : codigoCliente.entrySet()){
+            System.out.printf("Nombre cliente: %s - Código cliente: %s\n",pares.getKey(),pares.getValue());
+        }
+        System.out.println();
+        
+        System.out.println("MOSTRAR DATOS SOLO CLAVES");
+        for(String valor:codigoCliente.keySet()){
+            System.out.println(valor);
+        }
+        System.out.println();
+        
+        System.out.println("MOSTRAR DATOS SOLO VALORES");
+        for(String valor:codigoCliente.values()){
+            System.out.println(valor);
+        }
+        
+        
+        
+        
+        
     }
     
 }
