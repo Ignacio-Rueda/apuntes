68abfc3509211a7d683b982e7cd6dd91a3d58f77
Ignacio-Rueda <nachord@outlook.com>
2024-02-28 08:17:30 +0100

    Subida2

--- PROGRAMACIÓN/Tema_7/src/tema_7/Mapas/Mapas.java
+++ PROGRAMACIÓN/Tema_7/src/tema_7/Mapas/Mapas.java
@@ -4,7 +4,7 @@ package tema_7.Mapas;
 public class Mapas {
     public static void main(String[]args){
         System.out.println("hola ma!");
-        System.out.println("hola");
+        System.out.println("hola ");
     }
     
 }
